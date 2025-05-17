841aa8cdb9998c76b7bbd3374573daeba55b9799
luciagd2 <luciapgd2@gmail.com>
2025-05-17 11:05:28 +0200

    Primer commit

---
+++ src/main/java/com/tfg/tfgwebapp/TfgWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.tfgwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfgWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tfg/tfgwebapp/TfgWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.tfgwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
